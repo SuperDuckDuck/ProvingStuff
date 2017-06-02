b4340f570ce5e5cef40f7bd31182952e3255b239
Sven Wille <SvenWille@users.noreply.github.com>
2017-06-02 18:55:12 +0200

    empty message

--- Isabelle/Analysis/Reihen.thy
+++ Isabelle/Analysis/Reihen.thy
@@ -1,8 +1,8 @@
 theory Reihen
-imports Main
+imports Complex_Main
 begin 
 
-lemma   "(\<Sum>i=0 .. (n::nat) . i) = n*(n+1) div 2"
+lemma gauss :   "(\<Sum>i=0 .. (n::nat) . i) = n*(n+1) div 2"
 proof (induct n)
   case 0
   show ?case by simp 
@@ -27,7 +27,55 @@ proof (induct n)
   also from a have  " \<dots> =   ( (2*n+1)  + n^2 = n^2 + 2*n + 1)  " by auto
   finally show ?case by auto
 qed
+
+
+
+lemma fixes n::nat shows"(\<Sum>i = 1 .. n . i^2) = (n*(n + 1)*(2*n + 1)) div 6" 
+proof (induct n)
+  case 0
+  show ?case by simp
+ next 
+  fix n::nat
+  case (Suc n)
+  assume hyp:"(\<Sum>i = 1..n. i\<^sup>2) = (n * (n + 1) * (2 * n + 1)) div 6"
+ 
+  have "Suc n * (Suc n + 1) * (2 * Suc n + 1)   = (n + 1) * (n + 2) * (2 * n + 2 + 1) " by simp
+  also have " \<dots> =  (n^2 +3*n + 2) * (2 * n + 2 +1)" by (simp add : power2_eq_square)
+  also have "\<dots>  =  (n^2 +3*n + 2) * (2 * n + 3)"   
+  proof -
+    have tmp:"(2 * n + 2 +1) = (2 * n + 3)" by auto
+    show ?thesis by (simp only: tmp )
+  qed
+  also have "\<dots> =   (n^2 +3*n + 2)*2*n +   (n^2 +3*n + 2)*3" by (simp only : ring_distribs)
+  also have "\<dots> =  (n^2 +3*n + 2)*2*n +   3*n^2 +9*n + 6 " by (simp only : ring_distribs)
+  also have "\<dots>  = 2*(n^2)*n +6*n^2 + 4*n  + 3*n^2 +9*n + 6" by (simp add : ring_distribs power2_eq_square)
+  also have "\<dots> = 2*n^3 +9*n^2 + 13*n + 6 " by (simp add: power2_eq_square power3_eq_cube)
+  finally have " (Suc n * (Suc n + 1) * (2 * Suc n + 1)) div 6 =  (2 * n ^ 3 + 9 * n\<^sup>2 + 13 * n + 6) div 6" by simp
+  note tmpResult1 = this
+
+  have "(\<Sum>i = 1..Suc n. i\<^sup>2) =   (\<Sum>i = 1 .. (n + 1).  i\<^sup>2)" by simp 
+  also have "\<dots> = (n^2 + 2*n + 1)+ (\<Sum>i = 1 .. n.  i\<^sup>2)" by (simp add : power2_eq_square) 
+  also have "\<dots> = (6*(n^2 + 2*n + 1) div 6) + (\<Sum>i = 1 .. n.  i\<^sup>2)" by auto
+  also have "\<dots> = ((6*n^2 + 12*n + 6) div 6) + (\<Sum>i = 1 .. n.  i\<^sup>2)" by simp
+  also have "\<dots> =  ((6*n^2 + 12*n + 6) div 6) + (n * (n + 1) * (2 * n + 1)) div 6 " unfolding hyp by simp
+  also have "\<dots> = ((6*n^2 + 12*n + 6) + (n * (n + 1) * (2 * n + 1)))div 6 " by simp
+  also have "\<dots> = ((6*n^2 + 12*n + 6) + ((n^2 + n)* (2 * n + 1))) div 6" by (simp add : power2_eq_square ring_distribs)
+  also have "\<dots> = ((6*n^2 + 12*n + 6) + (2*n^3 + 3*n^2 + n)) div 6" by (simp add : power2_eq_square power3_eq_cube ring_distribs)
+  also  have "\<dots> = (2*n^3 + 9*n^2 + 13*n + 6) div 6 " by simp
+  note tmpResult2 =  calculation
+
+  from tmpResult1 and tmpResult2 show ?case by simp
+qed
   
+
+  
+
+
+  
+  
+    
+
+
 
 
 lemma " suminf (\<lambda>n. c^n) = (1 / (1 - c))"
--- Isabelle/ProbabilityTheory/ProbabilityTheory.thy
+++ Isabelle/ProbabilityTheory/ProbabilityTheory.thy
@@ -1,7 +1,6 @@
-theory Integrals
+theory ProbabilityTheory 
 imports Complex_Main 
 begin 
-
 
 
 
